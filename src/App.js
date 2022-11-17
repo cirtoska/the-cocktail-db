e151502a153b436377a4084f28856726eb62c305
Sashka.Chi <cirtoska@gmail.com>
2022-11-17 12:24:48 +0100

    fixing mobile responsive design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Cocktail from "./pages/SingleCocktail";
